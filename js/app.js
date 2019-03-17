7447ba9186cdc5d5f51826f208255e0e3c49de78
Cristian Pinto <cdpp5@hotmail.com>
2019-03-17 10:17:44 -0500

    Added Event Listener and Handler for Form

--- js/app.js
+++ js/app.js
@@ -3,8 +3,24 @@ const maxYear = new Date().getFullYear();
 const minYear = maxYear - 20;
 const years = document.querySelector('#anio');
 for (i=maxYear; i >= minYear; i--) {
-    let option = document.createElement('option');
-    option.value = i;
-    option.innerHTML = i;
-    years.appendChild(option)
+  let option = document.createElement('option');
+  option.value = i;
+  option.innerHTML = i;
+	years.appendChild(option)
 }
+
+// Variables
+const form = document.querySelector('#cotizar-seguro');
+const brand = document.querySelector('#marca');
+const membership = document.querySelector('input[name="tipo"]:checked');
+
+// Event Listeners
+form.addEventListener('submit', handleForm);
+
+function handleForm (e) {
+	e.preventDefault();
+	console.log(brand.value)
+	console.log(years.value)
+	console.log(membership.value)
+}
+
