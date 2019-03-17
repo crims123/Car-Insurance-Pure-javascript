7a7159d97a0e14d73f573be331597d83e5744c11
Cristian Pinto <cdpp5@hotmail.com>
2019-03-17 15:58:46 -0500

    Changed Prototypes for Classes

--- js/app.js
+++ js/app.js
@@ -36,81 +36,79 @@ function handleForm (e) {
 		interface.showResult(insurance, price);
 	}	
 }
-
-function Interface () {
-
-}
-// We add showError like a prototype
-Interface.prototype.showError = function (message) {
-	const div = document.createElement('div');
-	div.classList.add('mensaje', 'error');
-	div.innerHTML = message;
-	form.insertBefore(div, document.querySelector('.form-group'))
-	setTimeout(function() {
-		document.querySelector('.mensaje').remove()
-	}, 2000 )
-}
-// We add showResult like a prototype
-Interface.prototype.showResult = function (insurance, price) {
- const result = document.querySelector('#resultado');
- switch (insurance.brand) {
-	case '1':
-	insurance.brand = 'Americano';
-	break;
-
-	case '2':
-	insurance.brand = 'Asiatico';
-	break;
-
-	case '3':
-	insurance.brand = 'Europeo';
-	break;
- }
- const div = document.createElement('div');
- div.innerHTML = `
-	<p class="header">Tu Resumen:</p>
-	<p>Marca: ${insurance.brand}</p>
-	<p>Año: ${insurance.year}</p>
-	<p>Tipo: ${insurance.membership}</p>
-	<p>Total: $ ${price}</p>
- `
-	document.querySelector('#cargando img').style='display:block';
-	setTimeout(function () {
-		document.querySelector('#cargando img').style='display:none';
-		result.appendChild(div)
-	} ,1500)
-}
-
-function Insurance (brand, year, membership) {
-	this.brand = brand;
-	this.year = year;
-	this.membership = membership;
-}
-// We add price like a protoype
-Insurance.prototype.price = function (insurance) {
-	const base = 2000;
-	let price;
-	switch (insurance.brand){
-		case '1':
-		price = base * 1.15;
-		break;
-
-		case '2':
-		price =  base * 1.05;
-		break;
-
-		case '3':
-		price =  base * 1.35;
-		break;
+class Interface {
+	showError(message){
+		const div = document.createElement('div');
+		div.classList.add('mensaje', 'error');
+		div.innerHTML = message;
+		form.insertBefore(div, document.querySelector('.form-group'))
+		setTimeout(function() {
+			document.querySelector('.mensaje').remove()
+		},2000)
 	}
-	const diference = new Date().getFullYear() - Number(insurance.year);
-	const discount = diference * 0.03;
-	price = price -(price* discount);
-	if (insurance.membership === 'basico') {
-		price = price + (price* 0.3);
+
+	showResult(insurance, price){
+		const result = document.querySelector('#resultado');
+		switch (insurance.brand) {
+			case '1':
+			insurance.brand = 'Americano';
+			break;
+
+			case '2':
+			insurance.brand = 'Asiatico';
+			break;
+
+			case '3':
+			insurance.brand = 'Europeo';
+			break;
+		}
+		const div = document.createElement('div');
+		div.innerHTML = `
+			<p class="header">Tu Resumen:</p>
+			<p>Marca: ${insurance.brand}</p>
+			<p>Año: ${insurance.year}</p>
+			<p>Tipo: ${insurance.membership}</p>
+			<p>Total: $ ${price}</p>
+		`
+		document.querySelector('#cargando img').style='display:block';
+		setTimeout(function () {
+			document.querySelector('#cargando img').style='display:none';
+			result.appendChild(div)
+		},1500)
 	}
-	if (insurance.membership === 'completo') {
-		price = price + (price* 0.5);
+}
+class Insurance {
+	constructor(brand, year, membership){
+		this.brand = brand;
+		this.year = year;
+		this.membership = membership;
 	}
-	return price;
+	
+	price(insurance){
+		const base = 2000;
+		let price;
+		switch (insurance.brand){
+			case '1':
+			price = base * 1.15;
+			break;
+	
+			case '2':
+			price =  base * 1.05;
+			break;
+	
+			case '3':
+			price =  base * 1.35;
+			break;
+		}
+		const diference = new Date().getFullYear() - Number(insurance.year);
+		const discount = diference * 0.03;
+		price = price -(price* discount);
+		if (insurance.membership === 'basico') {
+			price = price + (price* 0.3);
+		}
+		if (insurance.membership === 'completo') {
+			price = price + (price* 0.5);
+		}
+		return price;
+	}
 }
